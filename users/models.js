95e291812576a44cd8b8a55fa7af133da0f70199
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-03-14 11:29:44 -0400

    Add user models

---
+++ users/models.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.plugin(AutoIncrement, {inc_field: 'userId'});
+
+exports.getUser = function() {
+    return mongoose.model("User", userSchema)
+}
+
